ba63a63ec5d6ff67202a790fbcc159948f7a81e6
Junya Ogura <juno@sooey.com>
2009-06-19 16:25:43 +0900

    removed unused code

--- lib/redmine_markdown_extra_formatter/wiki_formatter.rb
+++ lib/redmine_markdown_extra_formatter/wiki_formatter.rb
@@ -9,7 +9,6 @@ module RedmineMarkdownExtraFormatter
 
     def to_html(&block)
       BlueFeather.parse(@text.gsub(/\[(.+)\]\((https?:\/\/.+)\)/, '[\1](/redirect/\2)'))
-#      BlueFeather.parse(@text).gsub(/href=(["'])(.+)(["'])/i, 'href=\1/redirect/\2\3')
     rescue => e
       return("<pre>problem parsing wiki text: #{e.message}\n"+
              "original text: \n"+
