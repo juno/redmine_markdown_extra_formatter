8f08c0d629d367fa66fa6b62ded92e3a01dd7bc6
James Van Lommel <james.vanlommel@slo.courts.ca.gov>
2011-06-01 15:02:17 -0700

    Fix helper so CSS does get included in Redmine >= 1.2.0
    Changes in 1.2.0 now permit CSS to be included only on pages that have
    wiki editing boxes on them.
    See http://www.redmine.org/projects/redmine/repository/revisions/5239/entry/trunk/lib/redmine/wiki_formatting/textile/helper.rb
    
    Also changed back how we generate the URL to the syntax helper - it seems
    Rails 2.3.11 changes the previous behavior, and non-standard install locations
    get properly referenced.

--- app/helpers/redmine_markdown_extra_formatter/helper.rb
+++ app/helpers/redmine_markdown_extra_formatter/helper.rb
@@ -3,8 +3,13 @@ module RedmineMarkdownExtraFormatter
     unloadable
 
     def wikitoolbar_for(field_id)
-      url = Redmine::Utils.relative_url_root +
-        Engines::RailsExtensions::AssetHelpers.plugin_asset_path('redmine_markdown_extra_formatter', 'help', 'markdown_extra_syntax.html')
+      heads_for_wiki_formatter
+
+      # Only way we have to link to a public resource.(?)
+      #url = "#{Redmine::Utils.relative_url_root}/help/wiki_syntax.html"
+
+      url = Engines::RailsExtensions::AssetHelpers.plugin_asset_path('redmine_markdown_extra_formatter', 'help', 'markdown_extra_syntax.html')
+
       help_link = l(:setting_text_formatting) + ': ' +
         link_to(l(:label_help), url,
         :onclick => "window.open(\"#{url}\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
@@ -21,8 +26,14 @@ module RedmineMarkdownExtraFormatter
     end
 
     def heads_for_wiki_formatter
-      stylesheet_link_tag('jstoolbar') +
-        stylesheet_link_tag('markdown_extra', :plugin => 'redmine_markdown_extra_formatter')
+      unless @heads_for_wiki_formatter_included
+        content_for :header_tags do
+          stylesheet_link_tag('jstoolbar') +
+          stylesheet_link_tag('markdown_extra', :plugin => 'redmine_markdown_extra_formatter')
+        end
+        @heads_for_wiki_formatter_included = true
+      end
     end
+
   end
 end
